d237470208f23284f6a0d012d341e446936fb264
Ivan <githubh@gmail.com>
2016-10-02 19:26:33 +0300

    convert erb to haml

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,9 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.sass.preferred_syntax = :sass
-
 module Cmsruby
   class Application < Rails::Application
+  	config.sass.preferred_syntax = :sass
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
