1a0d470c366f74f2e3c1721ef3e9ccea7d3240d9
Ivan <githubh@gmail.com>
2016-10-10 22:48:54 +0300

    Change routes path

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -29,7 +29,7 @@ module Admin
 
       respond_to do |format|
         if @page.save
-          format.html { redirect_to @page, notice: 'Page was successfully created.' }
+          format.html { redirect_to admin_page_path(@page), notice: 'Page was successfully created.' }
           format.json { render :show, status: :created, location: @page }
         else
           format.html { render :new }
@@ -43,7 +43,7 @@ module Admin
     def update
       respond_to do |format|
         if @page.update(page_params)
-          format.html { redirect_to @page, notice: 'Page was successfully updated.' }
+          format.html { redirect_to admin_page_path(@page), notice: 'Page was successfully updated.' }
           format.json { render :show, status: :ok, location: @page }
         else
           format.html { render :edit }
@@ -57,7 +57,7 @@ module Admin
     def destroy
       @page.destroy
       respond_to do |format|
-        format.html { redirect_to pages_url, notice: 'Page was successfully destroyed.' }
+        format.html { redirect_to admin_pages_url, notice: 'Page was successfully destroyed.' }
         format.json { head :no_content }
       end
     end
